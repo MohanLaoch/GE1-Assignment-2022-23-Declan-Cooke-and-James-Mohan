c56e52bdaeddccee5ff15b0294e074e3d48d093a
declanqc <C20465442@mytudublin.ie>
2022-12-17 18:10:14 +0000

    added planet gravity to player

---
+++ GE1 2022-23 Assignment Project/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+    public GravityAttractor attractor;
+    private Transform myTransform;
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
