92172e8bca6cbb022b9cb7b37e4f9213cc16c472
declanqc <C20465442@mytudublin.ie>
2022-12-20 12:11:03 +0000

    add lerping to smoothen the audio game objects

--- GE1 2022-23 Assignment Project/Assets/Scripts/AudioTest.cs
+++ GE1 2022-23 Assignment Project/Assets/Scripts/AudioTest.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,53 +7,25 @@ using UnityEngine.UIElements;
 public class AudioTest : MonoBehaviour
 {
     public float scale = 10;
-    public Transform TestCloud;
-    List<GameObject> elements = new List<GameObject>();
-    // Use this for initialization
-    void Start()
-    {
-        CreateVisualisers();
+  
 
-    }
 
-    public float radius = 50;
-
-    void CreateVisualisers()
-    {
-        
-      /*  float theta = (Mathf.PI * 2.0f) / (float)AudioManager.frameSize;
-        for (int i = 0; i < AudioManager.frameSize; i++)
-        {
-            Vector3 p = new Vector3(
-                Mathf.Sin(theta * i) * radius
-                , 0
-                , Mathf.Cos(theta * i) * radius
-                );
-            p = transform.TransformPoint(p);
-            Quaternion q = Quaternion.AngleAxis(theta * i * Mathf.Rad2Deg, Vector3.up);
-            q = transform.rotation * q;
-
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.SetPositionAndRotation(p, q);
-            cube.transform.parent = this.transform;
-            cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(
-                i / (float)AudioManager.frameSize
-                , 1
-                , 1
-                );
-            elements.Add(cube);
-        }*/
-    }
-
+  
     
     void Update()
     {
         for (int i = 0; i < 10; i++)
         {
-            //elements[i].transform.localScale = new Vector3(1, 1 + AudioManager.Instance.spectrum[i] * scale, 1);
-            TestCloud.transform.localScale = new Vector3 (1, 1 + AudioManager.Instance.spectrum[i] * scale, 1);
-    }
-        
+            // this.gameObject.transform.localScale = new Vector3(1 + AudioManager.Instance.spectrum[i] * scale, 1 + AudioManager.Instance.spectrum[i] * scale, 1 + AudioManager.Instance.spectrum[i] * scale);
+            Vector3 ls = this.transform.localScale;
+            ls.x = Mathf.Lerp(ls.x, 1 + (AudioManager.Instance.bands[i] * scale), Time.deltaTime * 3f);
+            ls.y = Mathf.Lerp(ls.y, 1 + (AudioManager.Instance.bands[i] * scale), Time.deltaTime * 3f);
+            ls.z = Mathf.Lerp(ls.z, 1 + (AudioManager.Instance.bands[i] * scale), Time.deltaTime * 3f);
+
+            this.transform.localScale = ls;
+
+        }
+
 
     }
 }
