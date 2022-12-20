4a3e24754189e2a1ac97cb1efe6a2cd79cb2e08a
James Mohan <c20305011@mytudublin.ie>
2022-12-20 18:55:13 +0000

    added colour changer

--- GE1 2022-23 Assignment Project/Assets/Scripts/AudioTest.cs
+++ GE1 2022-23 Assignment Project/Assets/Scripts/AudioTest.cs
@@ -7,22 +7,25 @@ using UnityEngine.UIElements;
 public class AudioTest : MonoBehaviour
 {
     public float scale = 10;
-  
 
+    public GameObject[] scaleObjects;
 
-  
-    
     void Update()
     {
         for (int i = 0; i < 10; i++)
         {
             // this.gameObject.transform.localScale = new Vector3(1 + AudioManager.Instance.spectrum[i] * scale, 1 + AudioManager.Instance.spectrum[i] * scale, 1 + AudioManager.Instance.spectrum[i] * scale);
-            Vector3 ls = this.transform.localScale;
+            Vector3 ls = scaleObjects[i].transform.localScale;
             ls.x = Mathf.Lerp(ls.x, 1 + (AudioManager.Instance.spectrum[i] * scale), Time.deltaTime * 3f);
             ls.y = Mathf.Lerp(ls.y, 1 + (AudioManager.Instance.spectrum[i] * scale), Time.deltaTime * 3f);
             ls.z = Mathf.Lerp(ls.z, 1 + (AudioManager.Instance.spectrum[i] * scale), Time.deltaTime * 3f);
 
-            this.transform.localScale = ls;
+
+            foreach (GameObject scaleObjects in scaleObjects)
+            {
+                scaleObjects.transform.localScale = ls;
+            }
+            
 
         }
 
